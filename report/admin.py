4de8e95787e15e47165d46d5a908c0401c656aaf
SebastianSamanieg <sebastian_samaniego@outlook.com>
2023-11-22 21:13:19 -0500

    Se incluye html

--- report/admin.py
+++ report/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ConexionDB
 
-# Register your models here.
+
+class ConexionDBAdmin(admin.ModelAdmin):
+    list_display = ("name","host","user")
+    search_fields = ("name","host")
+
+admin.site.register(ConexionDB, ConexionDBAdmin)
--- report/models.py
+++ report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ConexionDB(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    host = models.CharField(max_length=100)
+    port = models.IntegerField()
+    name_db = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
